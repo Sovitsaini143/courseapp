33e112ad34ca1f34e84dc76dde4ea38a6b0ec07d
Sovit saini <sovitsaini37@gmail.com>
2025-05-10 11:22:40 +0530

    www

--- frontend/src/admin/AdminLogin.jsx
+++ frontend/src/admin/AdminLogin.jsx
@@ -1,45 +1,47 @@
-import React, { useState } from "react";
-import logo from "../../public/logo.webp";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-import toast from "react-hot-toast";
-import { BACKEND_URL } from "../utils/utils";
-
+import React, { useState } from 'react'
+import logo from "../../public/logo.webp"
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { BACKEND_URL } from '../utils/utils';
 function AdminLogin() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-  const [errorMessage, setErrorMessage] = useState("");
+ 
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-  const navigate = useNavigate();
+   const[errorMessage,setErrorMessage]=useState("");
 
-  const handleSubmit = async (e) => {
+  const navigate=useNavigate()
+
+
+  const handleSubmit= async (e) => {
     e.preventDefault();
-    console.log({ password });
-    try {
-      const response = await axios.post(
-          `${BACKEND_URL}/admin/login`,
-        {
-          email,
-          password,
+    
+
+    try{
+    const response= await axios.post(`${BACKEND_URL}/admin/login`,{
+       
+        email, 
+        password,
+      },{
+        withCredentials: true,
+        headers: {
+          "content-type": "application/json",
         },
-        {
-          withCredentials: true,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      console.log("AdminLogin successful: ", response.data);
+      })
+      console.log("Admin Login successfull:", response.data)
       toast.success(response.data.message);
-      navigate("/admin/dashboard");
-      localStorage.setItem("admin", JSON.stringify(response.data));
-    } catch (error) {
-      if (error.response) {
-        setErrorMessage(error.response.data.errors || "AdminLogin failed!!!");
-      }
+      localStorage.setItem("admin", JSON.stringify( response.data));
+      navigate("/admin/dashboard")
+
+    } catch(error) {
+       if(error.response){ 
+        setErrorMessage(error.response.data.errors || "Login failed!!")
+       }
     }
-  };
+
+  }
 
   return (
     <div className="bg-gradient-to-r from-black to-blue-950 ">
@@ -55,29 +57,30 @@ function AdminLogin() {
           <div className="flex items-center space-x-4">
             <Link
               to={"/admin/signup"}
-              className="bg-transparent border border-gray-500 py-2 px-4 rounded-md"
+              className="bg-transparent border border-gray-500 p-1 text-sm md:text-md md:py-2 md:px-4 rounded-md"
             >
               Signup
             </Link>
             <Link
               to={"/courses"}
-              className="bg-orange-500 py-2 px-4 rounded-md"
+              className="bg-orange-500 p-1 text-sm md:text-md md:py-2 md:px-4 rounded-md"
             >
               Join now
             </Link>
           </div>
         </header>
 
-        {/* AdminLogin Form */}
-        <div className="bg-gray-900 p-8 rounded-lg shadow-lg w-[500px] mt-20">
+        {/* Login Form */}
+        <div className="bg-gray-900 p-8 rounded-lg shadow-lg w-[500px] m-8 md:m-0 mt-20">
           <h2 className="text-2xl font-bold mb-4 text-center">
             Welcome to <span className="text-orange-500">CourseHaven</span>
           </h2>
           <p className="text-center text-gray-400 mb-6">
-            Log in to access admin dashboard!
+            Log in to access paid content!
           </p>
 
           <form onSubmit={handleSubmit}>
+          
             <div className="mb-4">
               <label htmlFor="email" className=" text-gray-400 mb-2">
                 Email
@@ -126,7 +129,7 @@ function AdminLogin() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default AdminLogin;
+export default AdminLogin
